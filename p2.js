c3b6dc2ec86ba821bfeb11b5ce4096432f8177fe
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2022-03-20 11:58:19 +0530

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,31 @@
+console.log("hi, this is prachi")
+//Operators in JavaScript
+//Arithmetic Opeartors
+var a=10
+var b=100
+console.log("The value of a*b is", a*b)
+
+//Assignment Operator
+var c = b
+c-=2
+console.log(c)
+
+//Comparison operator
+console.log(a == b)
+console.log(a <=b )
+
+//Logical Operators
+console.log("Logical AND")
+console.log(true && true)
+console.log(false && false)
+console.log(false && true)
+console.log(true && false)
+console.log("Logical OR")
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+console.log("Logical NOT")
+console.log(!false)
+console.log(!true)
+
